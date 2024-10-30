4ae653a5d4a053cca93129eb7d0db9361768179b
Breno0906 <breno.castro.barreto@escola.pr.gov.br>
2024-10-30 08:49:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function mostra resultado()
 var número1=10;
 var número2=15;
 var resultado=número1+número2
-document.getElementById(resultado).innerHTML=resultado
+document.getElementById('resultado').innerHTML=resultado
 }
